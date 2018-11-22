1c0a23e425922f57e6ee26cf281db09b34882d74
Diego Steiner <diego.steiner@u041.ch>
2018-11-22 17:10:57 +0100

    Add snow

--- src/index.php
+++ src/index.php
@@ -3,13 +3,14 @@
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Page Title</title>
+  <title>Weihnachtsgruss aus dem ZLI</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" media="screen" href="./assets/app.css" />
   <link href="https://fonts.googleapis.com/css?family=Concert+One|Fredoka+One|Rye|Source+Sans+Pro:300,300i,400,400i,600,600i" rel="stylesheet">
   <script src="./assets/app.js"></script>
 </head>
 <body>
+  <div class="snow"></div>
   <header>
     <a href="https://www.zli.ch" class="logo" target="_blank">
       <img src="assets/logo_zli_2_zu_1.png">
@@ -22,14 +23,13 @@
   <main>
     <img class="map" src="./assets/karte_2_zu_1.png">
     <div class="places">
-      <button style="left: 40%; top: 30%;" onclick="javascript:show(this);" data-target="#modal"><img src="assets/icons/icon_japan.png"></button>
-      <button style="left: 85%; top: 75%;" onclick="javascript:show(this);" data-target="#modal-australia"><img src="assets/icons/icon_australien.png"></button>
+      <button style="left: 40%; top: 30%;" data-target="#modal"><img src="assets/icons/icon_japan.png"></button>
+      <button style="left: 85%; top: 75%;" data-target="#modal-australia"><img src="assets/icons/icon_australien.png"></button>
     </div>
   </main>
   <footer>
-    Weihnachtsgruss 2018&nbsp;&nbsp;|&nbsp;&nbsp;&copy; Zürcher Lehrbetriebsverband ICT &nbsp;&nbsp;|&nbsp;&nbsp;<a href="">Kontakt</a> &nbsp;&nbsp;|&nbsp;&nbsp;<a href="">Impressum</a>
+    Weihnachtsgruss 2018&nbsp;&nbsp;|&nbsp;&nbsp;&copy; Zürcher Lehrbetriebsverband ICT &nbsp;&nbsp;|&nbsp;&nbsp;<a href="www.zli.ch" target="_blank">Kontakt</a> &nbsp;&nbsp;|&nbsp;&nbsp;<a href="">Impressum</a>
   </footer>
-
 <?php foreach (scandir('countries/') as $file): ?>
   <?php
 $contents = [];
@@ -45,43 +45,21 @@ preg_match("/<body class=\"modal\">\n(.*)\n<\/body>/sm", file_get_contents("coun
   </div>
 <?endforeach;?>
 
-  <div id='modal' class="modal-wrapper close">
-    <div class="modal">
-      <button class="modal-close close">×</button>
-      <h1>Hello, World!</h1>
-      <div class="container">
-        <div class='col one-half'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <div class='col one-half'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-      </div>
-      <hr>
-      <div class="container">
-        <div class='col one-third'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <div class='col two-thirds'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-      </div>
-      <hr>
-      <div class="container">
-        <div class='col two-thirds'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <div class='col one-third'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-
   <script>
     var modalWrappers = document.getElementsByClassName('modal-wrapper');
     var closeTriggers = document.getElementsByClassName('close');
+    var openTriggers = document.querySelectorAll('.places > button');
 
+    Array.from(openTriggers).forEach(function(t) {
+      t.addEventListener('click', function() {
+        let targetSelector = t.dataset["target"];
+        let targets = document.querySelectorAll(targetSelector);
+
+        Array.from(targets).forEach(function(target) {
+          target.style.display='block'
+        });
+       })
+    })
     Array.from(closeTriggers).forEach(function(t) {
       t.addEventListener('click', function() { hide() })
     })
@@ -94,15 +72,6 @@ preg_match("/<body class=\"modal\">\n(.*)\n<\/body>/sm", file_get_contents("coun
       Array.from(modalWrappers).forEach(function(m) {
         m.style.display='none';
       })
-    }
-
-    function show(element) {
-      let targetSelector = element.dataset["target"];
-      let targets = document.querySelectorAll(targetSelector);
-
-      Array.from(targets).forEach(function(t) {
-        t.style.display='block'
-      });
     }
   </script>
 
